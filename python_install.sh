193cf8e5621be29e7baf0330d3bab764f3acf808
Erik Smith <erik@cruiserhouse.com>
2021-04-10 09:29:53 -0700

    python_install

---
+++ python_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+VERSION=3.8.7
+
+curl -OL https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
+tar -zxvf Python-$VERSION.tgz
+cd Python-$VERSION
+
+./configure --prefix=$HOME/.local
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+make -j$CORES
+make install
+
