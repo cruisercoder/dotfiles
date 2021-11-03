17e5678e6f375f29ada51b7a3bf00d69405ce686
Erik Smith <erik.smith@AWKSOSX063.local>
2021-11-03 06:50:41 -0700

    vim stuff

--- install.sh
+++ install.sh
@@ -20,3 +20,10 @@ for dotfile in "${DOTFILES[@]}";do
 	rm -rf "${HOME}/.${dotfile}"
 	ln -sf "${DIR}/${dotfile}" "${HOME}/.${dotfile}"
 done
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
+
+
