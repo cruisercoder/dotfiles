848814d061deb5006b2208c42f37abc07295734a
Erik Smith <erik.smith@AWKSOSX063.local>
2021-11-14 10:35:33 -0800

    python_install with openssl for M1

--- python_install.sh
+++ python_install.sh
@@ -2,12 +2,23 @@
 
 VERSION=3.8.10
 
+unamestr=`uname`
+
 mkdir -p out && cd out
 curl -OL https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
 tar -zxvf Python-$VERSION.tgz
 cd Python-$VERSION
 
-./configure --prefix=$HOME/.local
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+# for mac with brew installed 
+./configure \
+  --prefix=$HOME/.local \
+  --with-openssl=$(brew --prefix openssl)
+elif [[ "$unamestr" == 'Linux' ]]; then
+./configure \
+  --prefix=$HOME/.local 
+fi
 
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 make -j$CORES
